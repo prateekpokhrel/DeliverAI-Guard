6870e67828500af4f0fee145974de19701ff5554
prateekpokhrel <prateekpokhrel000@gmail.com>
2026-05-15 17:01:09 +0545

    commit

--- deliverai-backend/src/main/java/com/deliverai/deliverai_backend/security/SecurityConfig.java
+++ deliverai-backend/src/main/java/com/deliverai/deliverai_backend/security/SecurityConfig.java
@@ -2,15 +2,10 @@ package com.deliverai.deliverai_backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.Customizer;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
